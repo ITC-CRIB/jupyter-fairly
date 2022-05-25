f69543235c82fb627152d7713b6d64f93a2338cc
Manuel G. Garcia <gilbert.mg22@gmail.com>
2022-05-25 12:17:38 +0200

    add tests for connection class

---
+++ jupyterfair/jupyterfair/tests/test_connection.py
@@ -0,0 +1,43 @@
+"""
+Test for the Connection class
+"""
+
+import unittest
+import os
+import requests
+from dotenv import load_dotenv
+
+
+from jupyterfair.core.connection import Connection
+
+
+
+class TestConnection(unittest.TestCase):
+
+    # fixtures
+    def setUp(self) -> None:
+        load_dotenv()
+        self.token = os.getenv('TOKEN')
+        self.base_url = "https://api.figshare.com/v2/account"
+        self.connection = Connection(self.base_url, self.token)
+        return super().setUp()
+
+    def test_get(self):
+        """ Test if the GET returns retuns a Requests response object and a success status code: 200"""
+        response = self.connection.get(self.base_url)
+        print(response.json())
+        self.assertIsInstance(response, requests.Response)
+        self.assertEqual(response.status_code, 200)
+        
+    def test_post(self):
+        """ Test if the POST method returns a success status code: 200"""
+        pass
+
+    # clean up action
+    def tearDown(self) -> None:
+        self.connection.close_connection()
+        return super().tearDown()
+
+
+if __name__ == '__main__':
+    unittest.main()
