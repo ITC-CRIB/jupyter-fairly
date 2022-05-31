3b91281d683510d54ca62141e5b523ad9a7acb51
Manuel Garcia <gilbert.mg22@gmail.com>
2022-05-31 12:28:47 +0200

    Backend Extension for 4TU repository (#18)
    * create python package structure
    
    * update ignores for jupyterfain lab extension
    
    * scafolding for jupyterlab 'server' extension
    
    * add instructions to setup dev environment
    
    * update ignores
    
    * add abstract class for data repository
    
    * update ignores
    
    * create connection class
    
    * add class for 4turesearch data
    
    * add post method to connection class
    
    * add tests for connection class
    
    * fix bug with initializing connection with token
    
    * ingore labextension compiling files
    
    * rename module for 4tu research data
    
    * fix bug: raise for status not working
    
    * update tests for Connection class
    
    * add handler for listing articles
    
    * add method for listing articles to 4tu class
    
    * fix issue with importing modules into labextension
    
    * fix handle for listing articles in 4tu account
    
    * delete token from code, import token from .env

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # JupyterFAIR
-JupyterFAIR aims to provide a tool for seamless integration of Jupyter-based research environments and research data repositories.
+ A jupyterLab extension for seamless integration of Jupyter-based research environments and research data repositories.
+
+## Set up Dev Environment
+
+1. Create a conda environment with
+    ```shell
+    conda create -n jupyterfair --override-channels --strict-channel-priority -c conda-forge -c nodefaults jupyterlab=3 cookiecutter nodejs jupyter-packaging git
+    ```
+2. Clone repository
+3. Activate conda environment
+4. Install, activate and build extension
+    ```shell
+    # Install package in development mode
+    pip install -e .
+    # Link your development version of the extension with JupyterLab
+    jupyter labextension develop . --overwrite
+    # Enable the server extension
+    jupyter server extension enable jlab_ext_example
+    # Rebuild extension Typescript source after making changes
+    jlpm run build
+    ```
+
---
+++ jupyterfair/jupyterfair/handlers.py
@@ -0,0 +1,50 @@
+import json
+import tornado
+import os
+from jupyter_server.base.handlers import APIHandler
+from jupyter_server.utils import url_path_join
+from dotenv import load_dotenv
+
+
+DOTENV_FILE = "/home/manuel/Documents/devel/JupyterFAIR/jupyterfair/jupyterfair/core/.env"
+load_dotenv(DOTENV_FILE)
+TOKEN = os.getenv('TOKEN')
+
+from jupyterfair.core.connection import Connection
+from jupyterfair.core.four_tu import TOKEN, FourTuResearchData
+
+class RouteHandler(APIHandler):
+    # The following decorator should be present on all verb methods (head, get, post,
+    # patch, put, delete, options) to ensure only authorized user can request the
+    # Jupyter server
+    @tornado.web.authenticated
+    def get(self):
+        self.finish(json.dumps({
+            "data": "This is /jupyterfair/get_example endpoint... Hey!--Manuel"
+        }))
+
+class TUHandler(APIHandler):
+    """Handler for the 4TU research data repository"""
+    @tornado.web.authenticated
+    def get(self):
+        """"List articles for in an account"""
+
+        # TODO: Secure token via config files or env-variables
+        BASE_URL = "https://api.figshare.com/v2/account"
+        # ===================================================
+        connection = Connection(BASE_URL, TOKEN)
+        repo_api = FourTuResearchData(connection)
+        list_of_articles=repo_api.list_articles()
+        self.finish(json.dumps(list_of_articles))
+
+
+def setup_handlers(web_app):
+    host_pattern = ".*$"
+
+    base_url = web_app.settings["base_url"]
+    route_pattern = url_path_join(base_url, "jupyterfair", "get_example")
+    list_articles = url_path_join(base_url, "jupyterfair", "list_articles")
+    handlers = [
+        (route_pattern, RouteHandler), (list_articles, TUHandler)
+    ]
+    web_app.add_handlers(host_pattern, handlers)
