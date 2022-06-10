d4b604f2aa70ea6193176a67e6d054c291db41df
jurra <jose.urra86@gmail.com>
2022-06-10 18:15:08 +0200

    Cleanened config file, removed comments

--- jupyterfair/jupyterfair/config.py
+++ jupyterfair/jupyterfair/config.py
@@ -6,8 +6,4 @@ load_dotenv('./.env')
 
 FOURTU_TOKEN = os.environ['FOURTU_TOKEN']
 ZENODO_TOKEN = os.environ['ZENODO_TOKEN']
-# BASE_URL = os.environ['BASE_URL']
 
-# Dev environment variables
-# ARTICLE_ID = os.environ['ARTICLE_ID']
-CHUNK_SIZE = 1048576
