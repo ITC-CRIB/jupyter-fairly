a91a640b82da6d44f7354d42d298a73da83f2d7e
jurra <jose.urra86@gmail.com>
2022-06-10 18:12:25 +0200

    Updated setup files with used dependencies

--- jupyterfair/setup.py
+++ jupyterfair/setup.py
@@ -57,7 +57,14 @@ setup_args = dict(
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     install_requires=[
-        "jupyter_server>=1.6,<2"
+        "jupyter_server>=1.6,<2",
+        "requests",
+        "python-dotenv",
+        'pytest',
+        "jupyter-packaging",
+        "nodejs"
+
+
     ],
     zip_safe=False,
     include_package_data=True,
