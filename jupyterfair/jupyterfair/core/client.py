70f3128cac4a4057886f0b8f7c8acdef0cd75404
jurra <jose.urra86@gmail.com>
2022-06-10 18:14:27 +0200

    Interface and implementtion to upload files to an archive working. Proper tests must still be written.

--- jupyterfair/jupyterfair/core/client.py
+++ jupyterfair/jupyterfair/core/client.py
@@ -5,16 +5,20 @@ Abstractions for Data Repository
 '''
 from abc import ABC, abstractmethod
 
+import os
 import json
 import requests
 from requests import Request, Session
 from requests.exceptions import HTTPError
+import hashlib
+import zipfile
+
 import jupyterfair.config as config
 
 
 class Client(ABC):
     """ Abstract class for research data repositories"""
-    # Reuse TCP connection
+    # We use sessions to reuse TCP connection
     session = Session()
 
     @property
@@ -27,17 +31,13 @@ class Client(ABC):
     def BASE_URL(cls):
         raise NotImplementedError
 
-    @abstractmethod
-    def create_deposition(self, **argv):
-        """Creates a new deposition"""
-        pass
 
     # @abstractmethod
     # def upload_data(self, **argv):
     #     """Uploads files to the data repository"""
         
     
-    def _raw_issue_request(self, method, url, data=None, binary=False):
+    def raw_issue_request(self, method, url, data=None, binary=False):
         ''' Private method that implements full request including headers, auth, etc
         It allows to reuse the token and base url on every call
         '''
@@ -52,7 +52,7 @@ class Client(ABC):
             except ValueError:
                 data = response
         except HTTPError as error:
-            print ('Caught an HTTPError: {}'.format(error.message))
+            print ('Caught an HTTPError: {}'.format(error))
             print ('Body:\n', response.content)
             raise
         return data
@@ -61,54 +61,87 @@ class Client(ABC):
         '''Generic interface to issue different API requests with any method
         GET, POST, PUT, DELETE
         '''
-        return self._raw_issue_request(method, self.BASE_URL.format(endpoint=endpoint), *args, **kwargs)
+        return self.raw_issue_request(method, self.BASE_URL.format(endpoint=endpoint), *args, **kwargs)
+    
+    def zip_data_dir(self, dir_path):
+        """Here we asume the user has created a directory with data that can be
+        zipped to be uploaded in one step
+        """
+        pass
+        
+        print("Compression finished")
 
+    def get_file_check_data(self, file_name, chunksize):
+        """md5 checksum and file size for a file"""
+        with open(file_name, 'rb') as fin:
+            md5 = hashlib.md5()
+            size = 0
+            data = fin.read(chunksize)
+            while data:
+                size += len(data)
+                md5.update(data)
+                data = fin.read(chunksize)
+            return md5.hexdigest(), size 
+    
+
+    # TODO: Must be abstract method
     def make_connection():
         pass
+    
+    # TODO: Must be abstract method
+    def close_connection(self):
+        self.session.close()
 
-    def close_connection():
-        pass
-
-    def get_record():
+    # TODO: Must be abstract method
+    def get_archive_byId():
         '''
         '''
         pass
-
-    def create_record(self, **kwargs):
+    
+    @abstractmethod
+    def create_archive(self, **kwargs):
         '''
         returns deposition url 
         ''' 
         pass
     
-
-    def write_rmetadata_record_locally():
+    @abstractmethod
+    def del_archive(self, **kwargs):
+        '''
+        '''
+        pass
+    
+    # TODO: Must be abstract method
+    def write_metadata_record_locally():
         '''
         writes yaml file compliant with dublin core metadata standard
 
         '''
         pass     
 
-    # Update
+    # TODO: Must be abstract method
     def load_local_metadata_record():
         '''
         loads 
         '''
         pass
     
-    
-    def update_record():
+    # TODO: Must be abstract method
+    def update_archive():
         '''
         Creates a new version of existing record
         '''
         pass
 
-    def upload_data():
+    @abstractmethod
+    def upload_data_to_archive():
         '''
         returns success
         '''
         pass
 
-    def publish():
+    # TODO: Must be abstract method
+    def publish_archive():
         '''
         Returns publication status and url/doi 
         and stores it locally in a metadata file...
