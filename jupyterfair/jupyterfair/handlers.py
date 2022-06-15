7c65cb985c77b48cdeb01a6cb55f15d4c94e5507
Manuel G. Garcia <gilbert.mg22@gmail.com>
2022-06-15 19:59:45 +0200

    enable example handler

--- jupyterfair/jupyterfair/handlers.py
+++ jupyterfair/jupyterfair/handlers.py
@@ -8,18 +8,16 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 
-# from jupyterfair.core.connection import Connection
-# from jupyterfair.core.four_tu import FourTuResearchData
 
-# class RouteHandler(APIHandler):
-#     # The following decorator should be present on all verb methods (head, get, post,
-#     # patch, put, delete, options) to ensure only authorized user can request the
-#     # Jupyter server
-#     @tornado.web.authenticated
-#     def get(self):
-#         self.finish(json.dumps({
-#             "data": "This is /jupyterfair/get_test endpoint... Hoora! It works!!!"
-#         }))
+class RouteHandler(APIHandler):
+    # The following decorator should be present on all verb methods (head, get, post,
+    # patch, put, delete, options) to ensure only authorized user can request the
+    # Jupyter server
+    @tornado.web.authenticated
+    def get(self):
+        self.finish(json.dumps({
+            "data": "This is /jupyterfair/get_test endpoint... Hoora! It works!!!"
+        }))
 
 # class TUHandler(APIHandler):
 #     """Handler for the 4TU research data repository"""
@@ -36,13 +34,12 @@ TOKEN = os.getenv('TOKEN')
 #         self.finish(json.dumps(list_of_articles))
 
 
-# def setup_handlers(web_app):
-#     host_pattern = ".*$"
+def setup_handlers(web_app):
+    host_pattern = ".*$"
 
-#     base_url = web_app.settings["base_url"]
-#     route_pattern = url_path_join(base_url, "jupyterfair", "get_example")
-#     list_articles = url_path_join(base_url, "jupyterfair", "list_articles")
-#     handlers = [
-#         (route_pattern, RouteHandler), (list_articles, TUHandler)
-#     ]
-#     web_app.add_handlers(host_pattern, handlers)
+    base_url = web_app.settings["base_url"]
+    route_pattern = url_path_join(base_url, "jupyterfair", "get_example")
+    handlers = [
+        (route_pattern, RouteHandler)
+    ]
+    web_app.add_handlers(host_pattern, handlers)
