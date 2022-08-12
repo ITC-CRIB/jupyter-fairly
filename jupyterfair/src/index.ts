eecdc8dbd1ad508624534a944c42631a35490c67
Manuel G. Garcia <gilbert.mg22@gmail.com>
2022-08-12 17:18:39 +0200

    add command palette to plugin

--- jupyterfair/src/index.ts
+++ jupyterfair/src/index.ts
@@ -5,6 +5,11 @@ import {
 
 import { ISettingRegistry } from '@jupyterlab/settingregistry';
 
+import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
+
+import { Widget } from '@lumino/widgets';
+
+// import handlers from Jupyter Server extension
 import { requestAPI } from './handler';
 
 /**
@@ -13,9 +18,38 @@ import { requestAPI } from './handler';
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyterfair:plugin',
   autoStart: true,
+  requires : [ICommandPalette],
   optional: [ISettingRegistry],
-  activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null) => {
+  activate: (app: JupyterFrontEnd, palette: ICommandPalette, settingRegistry: ISettingRegistry | null) => {
     console.log('JupyterLab extension jupyterfair is activated!');
+    console.log('ICommandPalette:', palette);
+
+    //Add command example
+    const {commands} = app;
+
+    const command = 'jlab-examples:command';
+
+    // add command
+    commands.addCommand(command, {
+      label: 'Execute jlab-examples:command Command',
+      caption: 'Execute jlab-examples:command Command',
+      execute: (args: any) => {
+        const orig = args['origin'];
+        console.log(`jlab-examples:command has been called from ${orig}.`);
+        if (orig !== 'init') {
+          window.alert(`jlab-examples:command has been called from ${orig}.`);
+        }
+      },
+    });
+
+    //call command execution
+    commands.execute(command, {origin: 'init'}).catch((reason) => {
+      console.error(
+        `An error occurred during the execution of jlab:examples:command.\n${reason}.`
+      );
+    });
+    
+    // End of add command example
 
     if (settingRegistry) {
       settingRegistry
@@ -28,7 +62,7 @@ const plugin: JupyterFrontEndPlugin<void> = {
         });
     }
 
-    requestAPI<any>('get_example')
+    requestAPI<any>('datasets')
       .then(data => {
         console.log(data);
       })
