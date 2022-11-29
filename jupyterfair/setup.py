da2acfcdcf3ea0ed649dabf36ca5a11de26df14a
Manuel G. Garcia <gilbert.mg22@gmail.com>
2022-11-29 02:54:52 +0100

    fix issue with version of request package

--- jupyterfair/setup.py
+++ jupyterfair/setup.py
@@ -59,7 +59,7 @@ setup_args = dict(
     packages=setuptools.find_packages(),
     install_requires=[
         "jupyter_server>=1.6,<2",
-        "requests==2.27.1",
+        "requests>=2.27.1",
         "python-dotenv==0.20.0",
         'pytest==7.1.2',
         "jupyter-packaging==0.12.0",
@@ -89,29 +89,29 @@ setup_args = dict(
 # Create directory with dummy files in tests/fixtures/project_example
 # This is to ensure that the project is testable
 # Make directory in ./tests/fixtures/project_example
-DUMMY_PROJECT = "jupyterfair/tests/fixtures/dummy_project"
-(HERE / DUMMY_PROJECT).mkdir(parents=True, exist_ok=True)
-# Create dummy files in ./tests/fixtures/project_example
+# DUMMY_PROJECT = "jupyterfair/tests/fixtures/dummy_project"
+# (HERE / DUMMY_PROJECT).mkdir(parents=True, exist_ok=True)
+# # Create dummy files in ./tests/fixtures/project_example
 
-def create_file(n, d, path, file_name):
-    '''
-    Makes a file in the given path containing data with random numbers
-    n: number of lines to make
-    d: number of characters to make each line
-    path: path to make file in
-    file_name: name of file to make
+# def create_file(n, d, path, file_name):
+#     '''
+#     Makes a file in the given path containing data with random numbers
+#     n: number of lines to make
+#     d: number of characters to make each line
+#     path: path to make file in
+#     file_name: name of file to make
 
-    example create a 100 MB file
-    create_file(10**5, 100, <target_path>, <file_name>")
-    example create a 1 GB file
-    create_file(10**6, 100, <target_path>, <file_name>")
-    '''
-    f = open(f'{path}/{file_name}', 'w')
-    for i in range(n):
-        nums = [str(round(random.uniform(0, 1000 * 1000), 3)) for j in range(d)]
-        f.write(' '.join(nums))
-        f.write('\n')
-    f.close()
+#     example create a 100 MB file
+#     create_file(10**5, 100, <target_path>, <file_name>")
+#     example create a 1 GB file
+#     create_file(10**6, 100, <target_path>, <file_name>")
+#     '''
+#     f = open(f'{path}/{file_name}', 'w')
+#     for i in range(n):
+#         nums = [str(round(random.uniform(0, 1000 * 1000), 3)) for j in range(d)]
+#         f.write(' '.join(nums))
+#         f.write('\n')
+#     f.close()
 
 try:
     from jupyter_packaging import (
@@ -132,15 +132,15 @@ except ImportError as e:
         raise e
 
 if __name__ == "__main__":
-    # if dummy project with dummy files doesnt exists, create it
-    if not (HERE / DUMMY_PROJECT).exists():
-        # Create small file
-        create_file(10, 100, HERE / DUMMY_PROJECT, "file_1MB.txt"))    
+    # # if dummy project with dummy files doesnt exists, create it
+    # if not (HERE / DUMMY_PROJECT).exists():
+    #     # Create small file
+    #     create_file(10, 100, HERE / DUMMY_PROJECT, "file_1MB.txt"))    
     
-        # Create 10 files of 100 MB each
-        for i in range(10):
-            # check if file exists
-            if not (HERE / DUMMY_PROJECT / f'file{i}.txt').exists():
-                create_file(10**5, 100, DUMMY_PROJECT, f'file{i}.txt')
+    #     # Create 10 files of 100 MB each
+    #     for i in range(10):
+    #         # check if file exists
+    #         if not (HERE / DUMMY_PROJECT / f'file{i}.txt').exists():
+    #             create_file(10**5, 100, DUMMY_PROJECT, f'file{i}.txt')
     
     setuptools.setup(**setup_args)
