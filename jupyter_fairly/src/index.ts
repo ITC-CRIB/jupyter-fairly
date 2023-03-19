549133485923b01a9e8a7f785ee3bac5e356f80b
Manuel G. Garcia <gilbert.mg22@gmail.com>
2023-03-19 21:03:08 +0100

    create new extension template

---
+++ jupyter_fairly/src/index.ts
@@ -0,0 +1,43 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { ISettingRegistry } from '@jupyterlab/settingregistry';
+
+import { requestAPI } from './handler';
+
+/**
+ * Initialization data for the jupyter-fairly extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'jupyter-fairly:plugin',
+  autoStart: true,
+  optional: [ISettingRegistry],
+  activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null) => {
+    console.log('JupyterLab extension jupyter-fairly is activated!');
+
+    if (settingRegistry) {
+      settingRegistry
+        .load(plugin.id)
+        .then(settings => {
+          console.log('jupyter-fairly settings loaded:', settings.composite);
+        })
+        .catch(reason => {
+          console.error('Failed to load settings for jupyter-fairly.', reason);
+        });
+    }
+
+    requestAPI<any>('get_example')
+      .then(data => {
+        console.log(data);
+      })
+      .catch(reason => {
+        console.error(
+          `The jupyter_fairly server extension appears to be missing.\n${reason}`
+        );
+      });
+  }
+};
+
+export default plugin;
